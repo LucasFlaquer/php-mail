56535b4852e09a75a2ed2d242cf1dce192c1e2fb
Lucas Flaquer <lucas.flaquer@vm2.com.br>
2019-11-27 11:49:40 -0300

    fazendo post com xmlhttprequest

--- main.js
+++ main.js
@@ -17,7 +17,23 @@ form.onsubmit = e => {
     loading.setAttribute('class', 'loading');
     loading.setAttribute('src', 'img/loading.gif');
     form.appendChild(loading);
-    enviar();    
+    //enviar();    
+    request()
+      .then(function(response) {
+        console.log('response:'+response);
+        obj = Object.values(JSON.parse(response));
+        if(obj[0] && obj[1]) {
+          console.log('deu certo');
+          document.querySelector('.loading').remove();
+          campos.forEach(element=> {
+            element.value = '';
+          });
+          setTimeout(function(){ alert("Formulário Enviado Com sucesso!"); }, 100);
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 };
 function validaForm() {
@@ -26,59 +42,107 @@ function validaForm() {
     if(!element.checkValidity()) {
       element.parentElement.classList.add('required');
       invalidos.push(element);
-      //console.log(element);
     }
   });
   if(invalidos.length>0)
   invalidos[0].focus();
 }
-
-
 document.addEventListener("DOMContentLoaded", function() {
   campos.forEach(element=> {
     element.addEventListener("keyup", function(e) {
       if(this.checkValidity()){
         this.parentElement.classList.remove('required');
-        //nvalidos.pop(element);
       }
     });
     element.onchange = function(e) {
       this.parentElement.classList.remove('required');
-      //invalidos.pop(element);
     };
   })
 });
-
+//ajax
 function enviar() {
-  $.ajax({
-    url:'enviar.php',
-    type: 'POST',
-    data: {
-    'nome': nome.value,
-    'email': email.value,
-    'tel': tel.value,
-    'estado': UF.value,
-    'cidade':cidade.value,
-    'mensagem':mensagem.value
-  },
-  success: function(response){
-    if(response){
-      obj = Object.values(JSON.parse(response));
-      if(obj[0] && obj[1]) {
-
-        console.log('deu certo');
-        
-        document.querySelector('.loading').remove();
-        campos.forEach(element=> {
-          element.value = '';
-        })
-        setTimeout(function(){ alert("Formulário Enviado Com sucesso!"); }, 100);
-      } else {
-        alert('ERRO: dados invalidos');
-      }
-
-    }else
-      console.log('erro');
+  const xml = new XMLHttpRequest();
+  xml.open('POST', 'enviar.php', true);
+  var data = {
+    nome: nome.value,
+    email: email.value,
+    tel: tel.value,
+    estado: UF.value,
+    cidade:cidade.value,
+    mensagem:mensagem.value
   }
+  xml.setRequestHeader('Content-Type', 'application/json');
+  xml.send(data);
+  xml.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log('recebido');
+        console.log(this);
+        console.log(this.response);
+    }
+  };
+  // $.ajax({
+  //   url:'enviar.php',
+  //   type: 'POST',
+  //   data: {
+  //   'nome': nome.value,
+  //   'email': email.value,
+  //   'tel': tel.value,
+  //   'estado': UF.value,
+  //   'cidade':cidade.value,
+  //   'mensagem':mensagem.value
+  // },
+  // success: function(response){
+  //   if(response){
+  //     obj = Object.values(JSON.parse(response));
+  //     if(obj[0] && obj[1]) {
+
+  //       console.log('deu certo');
+        
+  //       document.querySelector('.loading').remove();
+  //       campos.forEach(element=> {
+  //         element.value = '';
+  //       })
+  //       setTimeout(function(){ alert("Formulário Enviado Com sucesso!"); }, 100);
+  //     } else {
+  //       alert('ERRO: dados invalidos');
+  //     }
+
+  //   }else
+  //     console.log('erro');
+  // }
+  // });
+}
+var request = function() {
+  var data = {
+    nome: nome.value,
+    email: email.value,
+    tel: tel.value,
+    estado: UF.value,
+    cidade:cidade.value,
+    mensagem:mensagem.value
+  }
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'enviar.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8'); 
+    xhr.send(serialize(data));
+    console.log(xhr);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if(xhr.status === 200) {
+          resolve(xhr.response);
+        } else {
+          reject('errp de reqiosocao');
+        }
+      }
+    }
   });
+}
+serialize = function(obj) {
+  var str = [];
+  for (var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
 }
